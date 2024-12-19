131f09364ae0175e076b1099dcba07c9fcc2ab09
Chris <42316927+xiansnn@users.noreply.github.com>
2024-12-19 15:36:16 +0100

    update docs

--- docs/html/dir_d44c64559bbebec7f509842c48db8b23.js
+++ docs/html/dir_d44c64559bbebec7f509842c48db8b23.js
@@ -1,6 +1,5 @@
 var dir_d44c64559bbebec7f509842c48db8b23 =
 [
-    [ "control_event.h", "control__event_8h_source.html", null ],
     [ "probe.h", "probe_8h.html", "probe_8h" ],
     [ "switch_button.h", "switch__button_8h.html", "switch__button_8h" ]
 ];
